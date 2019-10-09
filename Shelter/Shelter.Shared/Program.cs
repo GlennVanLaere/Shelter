ce85db901ef36cccfa783e2c977f9f5d37221ca9
Yanis De Maesschalck <yanis705@icloud.com>
2019-10-09 15:26:43 +0200

    Update Program.cs

--- Shelter/Shelter.Shared/Program.cs
+++ Shelter/Shelter.Shared/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Shelter.Shared
 {
@@ -6,7 +7,66 @@ namespace Shelter.Shared
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var shelter = new Shelter()
+            {
+                Name = "Our shelter"
+            };
+
+            shelter.Cats = new List<Cat>();
+            shelter.Cats = new Cat() { Declawed = 0, Race = "naaktkat"});
+
+            shelter.Cats.ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Declawed} - {x.Race}");
+            });
         }
+    }
+
+    public class Shelter
+    {
+        public string Name { get; set; }
+    }
+
+    public class Person
+    {
+        public string FullName => $"{Lastname}, ${FirstName}";
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+
+    public class Manager
+    {
+
+    }
+
+    public class CareTaker
+    {
+        public Animal TakesCareOf { get; set; }
+    }
+
+    public class Administrator
+    {
+
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int DateOfBirth { get; set; }
+        public bool IsChecked { get; set; }
+        public bool KidFriendly { get; set; }
+        public DateTime Since { get; set; }
+    }
+
+    public class Cat
+    {
+        public bool Declawed { get; set; }
+        public string Race { get; set; }
+    }
+
+    public class Dog
+    {
+        public bool Barker { get; set; }
+        public string Race { get; set; }
     }
 }
