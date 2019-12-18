ba17f666c229c92d2f75611c1fe3bf200129995c
Amber <amber.waltens@hotmail.com>
2019-12-18 16:35:35 +0100

    other added

--- Shelter/MyAspMvc/Models/ShelterDatabase.cs
+++ Shelter/MyAspMvc/Models/ShelterDatabase.cs
@@ -31,9 +31,10 @@ namespace MyAspMvc.Models
                 {
                    new Cat{ Id = 1,Name = "Poes",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = false,Since = DateTime.Now,Declawed = true,Race = "Hairless Sphynx"},
                    new Cat{ Id = 2,Name = "Kity",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = false,Since = DateTime.Now,Declawed = true,Race = "Hairless Sphynx"},
-                   new Cat{ Id = 3,Name = "wietel",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = false,Since = DateTime.Now,Declawed = true,Race = "Hairless Sphynx"},
+                   new Cat{ Id = 3,Name = "Wietel",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = false,Since = DateTime.Now,Declawed = true,Race = "Hairless Sphynx"},
                    new Dog{ Id = 4,Name = "Felix",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = true,Since = DateTime.Now,Barker = true,Race = "Golden Retriever"},
-                   new Dog{ Id = 5,Name = "peppa",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = true,Since = DateTime.Now,Barker = true,Race = "Danish Dog"},
+                   new Dog{ Id = 5,Name = "Peppa",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = true,Since = DateTime.Now,Barker = true,Race = "Danish Dog"},
+                   new Other{ Id = 6,Name = "Weirdo",DateOfBirth = new DateTime(2000, 02, 14),IsChecked = true,KidFriendly = true,Since = DateTime.Now ,Race = "other"},
                 };
             _context.Add(shelter);
             _context.SaveChanges();
